fe6becb6e34eb786d6376fc0d0abb6b168207d87
jmfarthin <122315957+jmfarthin@users.noreply.github.com>
2023-03-31 18:56:56 -0400

    completed newLogo function and tied into inquirer prompt--app now creates svg on running index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const maxLengthInputPrompt = require('inquirer-maxlength-input-prompt')
 const shape = require('./lib/shape')
-
 inquirer.registerPrompt('maxlength-input', maxLengthInputPrompt)
 
 const questions = [
@@ -10,7 +9,7 @@ const questions = [
         type: 'maxlength-input',
         name: 'text',
         message: 'Enter three characters for your logo text:',
-        maxLength: 3
+        maxLength: 3,
     },
     {
         type: 'input',
@@ -18,21 +17,25 @@ const questions = [
         message: 'Enter a color for your logo text:'
     },
     {
+        type: 'input',
+        name: 'fillColor',
+        message: 'Enter a color for your logo shape:'
+    },
+    {
         type: 'list',
         name: 'shape',
         message: 'Select a shape for your logo:',
         choices: ['triangle', 'square', 'circle']
-    },
-    {
-        type: 'input',
-        name: 'shapeColor',
-        message: 'Enter a color for your logo shape:'
     }
+
 ]
 
 
-const prompt = inquirer.createPromptModule();
 
-prompt(questions).then((res) => {
-    console.log(res);
-});
+inquirer
+    .prompt(questions).then((res) => {
+        console.log(res);
+        fs.writeFile('examples/newLogo.svg', shape.newLogo(res), err => {
+            err ? console.log(err) : console.log('created svg file')
+        })
+    });
